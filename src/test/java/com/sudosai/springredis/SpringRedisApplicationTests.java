979cbda3b0a4aad2cc9e9c41c35b28ef64670039
saisubham <subhamsaibehera11@gmail.com>
2022-01-13 21:06:06 +0530

    Changed @SpringBootTest to @DataJpaTest

--- src/test/java/com/sudosai/springredis/SpringRedisApplicationTests.java
+++ src/test/java/com/sudosai/springredis/SpringRedisApplicationTests.java
@@ -1,13 +1,15 @@
 package com.sudosai.springredis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@DataJpaTest
 class SpringRedisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
